0d1e43275c03263cc32cdce4430a2122ad846a38
oberdran123@gmail.com <oberdran123@gmail.com>
2024-12-11 17:19:34 -0300

    criado pacote service e classe categoryservice

---
+++ backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscatalog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+}
